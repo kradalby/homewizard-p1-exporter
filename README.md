e306bae2b4bd4dff36b7ff1ffab18662eae9ec7d
Kristoffer Dalby <kristoffer@dalby.cc>
2024-01-07 17:58:51 +0100

    initial commit
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ README.md
@@ -0,0 +1,41 @@
+# homewizard-p1-exporter
+
+A [Prometheus](prometheus.io) exporter that translates between Prometheus and [Homewizard P1](https://www.homewizard.com/p1-meter/) power tracker (plugs into your electrical meter).
+
+It is implemented in a very similar way to [blackbox_exporter](https://github.com/prometheus/blackbox_exporter) which allows you to run it
+as a "proxy" querying the power meter on the go and returns specific metrics for that power meter using targets and relabeling.
+
+## Tested with
+
+- Avatar UK 10A
+- Athom Plug V2
+
+## Configuration
+
+homewizard-p1-exporter does not need any configuration itself, and the seperation of power meter are fully hosted in the Prometheus
+scrape config.
+
+Just run the binary somewhere where it can reach the power meter over IP and from Prometheus.
+By default, port `9090` will be used, but this can be changed `HOMEWIZARD_EXPORTER_LISTEN_ADDR` on
+the format `:9111`.
+
+In Prometheus, add a new scrape target:
+
+```yaml
+scrape_configs:
+  - job_name: homewizard
+    metrics_path: /probe
+    static_configs:
+      - targets:
+          # add the address of your meter(s))here
+          - p1-power.local
+    relabel_configs:
+      - source_labels: [__address__]
+        target_label: __param_target
+      - source_labels: [__param_target]
+        target_label: instance
+      - target_label: __address__
+        replacement: 127.0.0.1:9090 # address of exporter
+```
+
+I recommend to have DNS names assigned to your meter so the instance name will be human readable.
---
+++ flake.nix
@@ -0,0 +1,123 @@
+{
+  description = "homewizard-p1-exporter";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+    ...
+  }: let
+    homewizard-p1-exporterVersion =
+      if (self ? shortRev)
+      then self.shortRev
+      else "dev";
+  in
+    {
+      overlay = _: prev: let
+        pkgs = nixpkgs.legacyPackages.${prev.system};
+      in {
+        homewizard-p1-exporter = pkgs.buildGoModule {
+          pname = "homewizard-p1-exporter";
+          version = homewizard-p1-exporterVersion;
+          src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+
+          subPackage = ["cmd/homewizard-p1-exporter"];
+
+          vendorHash = "sha256-LUHMUtClxEnizfxWmpMXSZA48f8ysNozuU5Gp4khMxI=";
+        };
+      };
+    }
+    // utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        overlays = [self.overlay];
+        inherit system;
+      };
+      buildDeps = with pkgs; [
+        git
+        gnumake
+        go
+      ];
+      devDeps = with pkgs;
+        buildDeps
+        ++ [
+          golangci-lint
+          entr
+          nodePackages.tailwindcss
+        ];
+    in rec {
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        buildInputs = devDeps;
+      };
+
+      # `nix build`
+      packages = with pkgs; {
+        inherit homewizard-p1-exporter;
+      };
+
+      defaultPackage = pkgs.homewizard-p1-exporter;
+
+      # `nix run`
+      apps = {
+        homewizard-p1-exporter = utils.lib.mkApp {
+          drv = packages.homewizard-p1-exporter;
+        };
+      };
+
+      defaultApp = apps.homewizard-p1-exporter;
+
+      overlays.default = self.overlay;
+    })
+    // {
+      nixosModules.default = {
+        pkgs,
+        lib,
+        config,
+        ...
+      }: let
+        cfg = config.services.homewizard-p1-exporter;
+      in {
+        options = with lib; {
+          services.homewizard-p1-exporter = {
+            enable = mkEnableOption "Enable homewizard-p1-exporter";
+
+            package = mkOption {
+              type = types.package;
+              description = ''
+                homewizard-p1-exporter package to use
+              '';
+              default = pkgs.homewizard-p1-exporter;
+            };
+
+            listenAddr = mkOption {
+              type = types.str;
+              default = ":9090";
+            };
+          };
+        };
+        config = lib.mkIf cfg.enable {
+          systemd.services.homewizard-p1-exporter = {
+            enable = true;
+            script = ''
+              export HOMEWIZARD_EXPORTER_LISTEN_ADDR=${cfg.listenAddr}
+              ${cfg.package}/bin/homewizard-p1-exporter
+            '';
+            wantedBy = ["multi-user.target"];
+            after = ["network-online.target"];
+            serviceConfig = {
+              DynamicUser = true;
+              Restart = "always";
+              RestartSec = "15";
+            };
+            path = [cfg.package];
+          };
+        };
+      };
+    };
+}
